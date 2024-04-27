5a12e29e098a00149709b73db6da288c9885bce2
Claudio Klingler <ck@realtime-projects.com>
2024-04-27 17:11:07 +0200

    fix step.

--- src/acre/steps/navigation.py
+++ src/acre/steps/navigation.py
@@ -5,5 +5,5 @@ from acre import controls
 
 @when("I click the link {link:QuotedString}")
 def i_click_the_link(step, link):
-    link = Link(text=link)
+    link = controls.Link(text=link)
     link.click()
